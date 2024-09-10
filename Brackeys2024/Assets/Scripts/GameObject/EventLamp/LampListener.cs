e9ccd7ef4c32f85c646c5266fa074e6d843dfe74
LeafkingOfficial <210522059@qq.com>
2024-09-11 00:17:05 +0800

    LampShine
    Adding LampShining Event in Lamptest scene

---
+++ Brackeys2024/Assets/Scripts/GameObject/EventLamp/LampListener.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using static UnityEngine.Rendering.DebugUI;
+
+[RequireComponent(typeof(EventObject))]
+public class LampListener : MonoBehaviour
+{
+    private EventObject @event;
+    public GameObject Spotlight;
+    public bool isVisible = true;
+    private Renderer objectRenderer;
+    public float randomInterval;
+    private float timer = 0;
+    private AudioSource audioSource;
+
+
+
+    private void Start()
+    {
+        @event = GetComponent<EventObject>();
+        objectRenderer = GetComponent<Renderer>();
+        audioSource =gameObject.GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= randomInterval)
+        {
+            RandomBlink();
+            timer = 0f;
+        }
+    }
+    private void RandomBlink() {
+        if (@event.IsEventOn)
+        {
+            bool vis = Random.Range(0, 2) == 0;
+            if (vis)
+            {
+                Spotlight.SetActive(false);
+                objectRenderer.material.DisableKeyword("_EMISSION");
+            }
+            else
+            {
+                Spotlight.SetActive(true);
+                objectRenderer.material.EnableKeyword("_EMISSION");
+            }
+            
+        }
+        else
+        {
+            Spotlight.SetActive(true);
+            objectRenderer.material.EnableKeyword("_EMISSION");
+            
+        }
+    }
+}
