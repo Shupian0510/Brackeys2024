eb5817b8422f76bdf0bb580bfbda898efd2d4c26
volticx <volticxmusic@163.com>
2024-09-15 06:06:00 +0800

    added notebook UI

---
+++ Brackeys2024/Assets/noteUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class noteUI : MonoBehaviour
+{
+    // 笔记本 UI 的 GameObject
+    public GameObject notebookUI;
+
+    // 玩家角色的引用
+    private Character playerCharacter;
+
+    // 笔记本 UI 的初始状态
+    private bool isNotebookOpen = false;
+
+    void Start() {
+        // 获取玩家角色的引用
+        playerCharacter = Character.Instance;
+    }
+
+    void Update() {
+        // 检查是否按下了 ESC 键
+        if (Input.GetKeyDown(KeyCode.Escape) && isNotebookOpen) {
+            // 切换笔记本 UI 的状态
+            ToggleNotebook();
+        }
+    }
+
+    // 切换笔记本 UI 的显示状态
+    private void ToggleNotebook() {
+        isNotebookOpen = !isNotebookOpen;
+        notebookUI.SetActive(isNotebookOpen);
+
+        if (isNotebookOpen) {
+            // 锁定玩家视角
+            playerCharacter.LockCamera(playerCharacter.transform);
+        }
+        else {
+            // 解锁玩家视角
+            playerCharacter.RestoreCamera();
+        }
+    }
+}
+
